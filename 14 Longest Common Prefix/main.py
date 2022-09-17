2192ea3965aa44e054381994615cd23648d50bb1
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2022-09-18 00:29:35 +0800

    2nd solution for Problem 14

--- 14 Longest Common Prefix/main.py
+++ 14 Longest Common Prefix/main.py
@@ -5,7 +5,7 @@ class Solution:
 
         r = ""
         s1 = strs.pop()
-        for i in range(1, len(s1)+1):
+        for i in range(1, len(s1) + 1):
             lookup = [False for _ in range(len(strs))]
 
             for idx, s in enumerate(strs):
@@ -21,7 +21,22 @@ class Solution:
 
         return r
 
+    def longestCommonPrefix2(self, strs: list[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+
+        strs.sort()
+        s = strs[0]
+        r = ""
+        for c in s:
+            if strs[-1].startswith(r + c):
+                r += c
+            else:
+                break
+        return r
+
 
 a = Solution()
-r = a.longestCommonPrefix(["flower","flow","flight"])
+r = a.longestCommonPrefix(["flower", "flow", "flight"])
+r = a.longestCommonPrefix2(["flower", "flow", "flight"])
 print(r)
