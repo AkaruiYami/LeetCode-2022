071202bbc6222aae946d9ef54f6b10471c87f076
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2022-09-23 21:02:45 +0800

    2nd solution for problem 17

--- 17 Letter Combinations of a Phone Number/main.py
+++ 17 Letter Combinations of a Phone Number/main.py
@@ -1,4 +1,15 @@
 class Solution:
+    lookup = {
+        "2": list("abc"),
+        "3": list("def"),
+        "4": list("ghi"),
+        "5": list("jkl"),
+        "6": list("mno"),
+        "7": list("pqrs"),
+        "8": list("tuv"),
+        "9": list("wxyz"),
+    }
+
     def combine(self, l1, l2):
         return [a + b for a in l1 for b in l2]
 
@@ -7,25 +18,25 @@ class Solution:
         if digits == "":
             return []
 
-        lookup = {
-            "2": list("abc"),
-            "3": list("def"),
-            "4": list("ghi"),
-            "5": list("jkl"),
-            "6": list("mno"),
-            "7": list("pqrs"),
-            "8": list("tuv"),
-            "9": list("wxyz"),
-        }
-
         if len(digits) == 1:
-            return lookup[digits]
+            return self.lookup[digits]
 
-        r = self.combine(lookup[digits[0]], self.letterCombinations(digits[1:]))
+        r = self.combine(self.lookup[digits[0]], self.letterCombinations(digits[1:]))
 
+        return r
+
+    def letterCombinations2(self, digits: str) -> list[str]:
+        if digits == "":
+            return []
+
+        r = [""]
+        for digit in digits:
+            r = [a + b for a in r for b in self.lookup[digit]]
         return r
 
 
 a = Solution()
 r = a.letterCombinations("23")
+r2 = a.letterCombinations2("23")
 print(r)
+print(r2)
